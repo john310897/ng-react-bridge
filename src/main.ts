aea65b93402f140acf31dfa24cac7d14d711adda
john <john310897@gmail.com>
2024-11-28 23:22:50 +0530

    added react component directive

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, inject, Input, OnChanges, OnDestroy, NgZone } from "@angular/core";
+import { createElement, ElementType } from "react";
+import { createRoot, Root } from "react-dom/client";
+
+@Directive({
+    selector: "[reactComponent]",
+    standalone: true,
+})
+export class ReactComponentDirective<Comp extends ElementType> implements OnChanges, OnDestroy {
+    @Input() reactComponent!: Comp;
+    @Input() props: { [key: string]: any; } | undefined
+    private root: Root;
+    private ngZone = inject(NgZone);
+
+    constructor() {
+        const elementRef = inject(ElementRef);
+        this.root = createRoot(elementRef.nativeElement);
+    }
+
+    ngOnChanges() {
+        this.ngZone.runOutsideAngular(() => {
+            if (this.reactComponent) {
+                this.root.render(createElement(this.reactComponent, this.props));
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.ngZone.runOutsideAngular(() => {
+            this.root.unmount();
+        });
+    }
+}
